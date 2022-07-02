605fc69a9d14a9184b70f0e84e8c163f731be667
Anahid <anahidhemmat2130@gmail.com>
2022-07-02 20:09:35 +0400

    editing todo list component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 
-const TodoForm = () => {
+const TodoForm = (props) => {
   const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+
+    setInput("");
   };
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
@@ -13,6 +24,7 @@ const TodoForm = () => {
         placeholder="Add a todo"
         value={input}
         className="todo-input"
+        onChange={handleChange}
       />
       <button className="todo-button">Add Todo</button>
     </form>
