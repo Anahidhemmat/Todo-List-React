7d0f38fd06a4175d0171bc400aba259c6c84c9df
Anahid <anahidhemmat2130@gmail.com>
2022-07-03 00:28:15 +0400

    edit function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -12,9 +12,19 @@ const TodoList = () => {
 
     setTodos(newTodos);
   };
+
   const removeTodo = (id) => {
     const removeArr = [...todos].filter((todo) => todo.id !== id);
     setTodos(removeArr);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
   };
   const completeTodo = (id) => {
     let updatedTodos = todos.map((todo) => {
@@ -30,7 +40,12 @@ const TodoList = () => {
     <div>
       <h1>What's the Plan for Today? </h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 };
