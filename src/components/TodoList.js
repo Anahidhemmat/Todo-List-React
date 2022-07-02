986798178e408810d431041df77fc877e113ce87
Anahid <anahidhemmat2130@gmail.com>
2022-07-02 21:41:05 +0400

    editing Todo component, remove todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -11,10 +12,25 @@ const TodoList = () => {
 
     setTodos(newTodos);
   };
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
       <h1>What's the Plan for Today? </h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
     </div>
   );
 };
